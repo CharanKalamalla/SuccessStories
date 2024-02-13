af5624955c13495027b314286aaa18e0d8e127c0
CharanKalamalla <145537045+CharanKalamalla@users.noreply.github.com>
2024-02-13 08:21:34 +0530

    Success

---
+++ successstories/src/App.js
@@ -0,0 +1,10 @@
+import Testimonials from "./components/Testimonials"
+import './App.css';
+
+function App() {
+  return (
+    <Testimonials/>
+  );
+}
+
+export default App;
---
+++ successstories/src/components/Slider/styledComponents.js
@@ -0,0 +1,70 @@
+import Styled from "styled-components";
+
+export const MainContainer = Styled.div`
+display: flex;
+flex-direction:column;
+padding: 10px;
+height:400px;
+margin-top:30px;
+width:300px;
+`
+export const CardContainer = Styled.div`
+background: #121415;
+border-radius:15px;
+padding: 20px;
+border:0px;
+`
+export const Stars = Styled.a`
+font-size:10px;
+`
+export const Para1 = Styled.p`
+color:#B7B6BD;
+font-family: "Inter", serif sans;
+font-size: 18px;
+font-weight: 700;
+line-height: 33px;
+letter-spacing: 0px;
+text-align: left;
+width: 368px
+height: 146px
+top: 1580px
+left: 139px
+
+`
+export const Para2 = Styled.p`
+color:#B7B6BD;
+font-family: "Inter", serif sans;
+font-size: 15px;
+font-weight: 400;
+line-height: 38px;
+letter-spacing: 0px;
+text-align: left;
+
+
+
+`
+export const ProfileCon = Styled.div`
+display: flex;
+flex-direction:row;
+gap:20px;
+
+`
+export const Image = Styled.img`
+
+`
+export const Details = Styled.div`
+display: flex;
+flex-direction:column;
+
+`
+export const Name = Styled.a`
+color:#2B459B;
+font-size:15px;
+`
+export const Role = Styled.a`
+color:#2B459B;
+font-size:12px;
+;
+
+
+`
